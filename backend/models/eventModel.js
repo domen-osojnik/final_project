8425c38014bab08f9ee6ecf597b03d6014ea95e4
galglogovsek <glogovsekgal@gmail.com>
2020-05-17 21:58:26 +0200

    Izbrisal nepotrebne modele in metode

--- backend/models/eventModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
-
-var eventScheme = new Schema({
-	'naziv' : String,
-	'lokacijaID' : { type: Schema.Types.ObjectId, ref: 'location' }
-});
-
-module.exports = mongoose.model('event', eventScheme); //DOGODEK
--- backend/models/testModel.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
-
-var testSchema = new Schema({
-	'msg' : String
-});
-
-module.exports = mongoose.model('test', testSchema);
