b03b7b46a861cdaf709401546be792830ff17221
Domen Osojnik <osojnikd@gmail.com>
2020-05-17 15:00:02 +0200

    Api klici, error handling, modeli za podatke

---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SensorData, ScrapeData, SignData } from '../viewmodels/interfaces';
+import { catchError, tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  public static dataHost = "http://localhost:3000/";
+
+  constructor(private http: HttpClient) { }
+
+   //Pridobivanje Logov
+   public getLogData(): Observable<SensorData[]> {
+    return this.http.get<SensorData[]>(ApiService.dataHost + "logData").pipe(
+      tap(() => {}),
+      catchError(this.handleError("Log data rertrieval", "404 NOT FOUND"))
+    );
+  }
+
+     //Pridobivanje Logov
+     public getSignData(): Observable<SignData[]> {
+      return this.http.get<SignData[]>(ApiService.dataHost + "signData").pipe(
+        tap(() => {}),
+        catchError(this.handleError("Sign data rertrieval", "404 NOT FOUND"))
+      );
+    }
+
+       //Pridobivanje Logov
+   public getScrapeData(): Observable<ScrapeData[]> {
+    return this.http.get<ScrapeData[]>(ApiService.dataHost + "scrapeData").pipe(
+      tap(() => {}),
+      catchError(this.handleError("Scrape data rertrieval", "404 NOT FOUND"))
+    );
+  }
+
+  //Error handling
+  private handleError(operation: String, error: string) {
+    return (err: any) => {
+      let errMsg = `error in ${operation}() retrieving ${ApiService.dataHost}`;
+      console.log(`${errMsg}:`, err);
+      if (err instanceof HttpErrorResponse) {
+        // you could extract more info about the error if you want, e.g.:
+        console.log(`status: ${err.status}, ${err.statusText}`);
+        // errMsg = ...
+      }
+      return Observable.throw(errMsg);
+    };
+  }
+}
---
+++ frontend/src/app/viewmodels/interfaces.ts
@@ -0,0 +1,26 @@
+export interface SensorData{
+    date: string;
+    latitude: number;
+    longtitude: number;
+    speed: number;
+    maxSpeed:number;
+    shakeDegree:number;
+}
+
+export interface SignData{
+    date: string;
+    latitude: number;
+    longtitude: number;
+    speed: number;
+    maxSpeed:number;
+    shakeDegree:number;
+}
+
+export interface ScrapeData{
+    date: string;
+    latitude: number;
+    longtitude: number;
+    speed: number;
+    maxSpeed:number;
+    shakeDegree:number;
+}
