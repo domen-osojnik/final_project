e3d73a220a4611898991bfcd55619b8babd24a4b
Domen Osojnik <osojnikd@gmail.com>
2020-05-17 14:49:47 +0200

    Api service
    Preurejena tabela, dodan api service

--- frontend/src/app/tabs/drive-information/drive-information.component.ts
+++ frontend/src/app/tabs/drive-information/drive-information.component.ts
@@ -4,24 +4,17 @@ interface Log {
   value: string;
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface SensorReading {
+  date: string;
+  latitude: number;
+  longtitude: number;
+  speed: number;
+  maxSpeed:number;
+  shakeDegree:number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: SensorReading[] = [
+  {date: new Date().getDate().toLocaleString(), latitude: 1.0079, longtitude: 1.0079, speed: 0, maxSpeed: 0, shakeDegree:0}
 ];
 
 @Component({
@@ -36,12 +29,11 @@ export class DriveInformationComponent implements OnInit {
     {value: 'ID12421525-2'}
   ];
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['date', 'latitude', 'longtitude', 'speed', 'maxSpeed', 'shakeDegree'];
   dataSource = ELEMENT_DATA;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
